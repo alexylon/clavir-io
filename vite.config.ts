11c7fd53d1f25a9ad50d0b3a75331cbec2817179
Alexander Alexandrov <git@alexo.io>
2026-03-11 21:36:23 +0200

    replace prerender plugin with lightweight post-build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { vitePrerenderPlugin } from 'vite-prerender-plugin'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePrerenderPlugin({
-      renderTarget: '#root',
-      prerenderScript: './src/prerender.tsx',
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 8080,
   },
